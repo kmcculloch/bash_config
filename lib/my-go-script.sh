d95a35add7a89e77dc3071daa19a203e9685f367
Kevin McCulloch <kevin_mcculloch@ao.uscourts.gov>
2015-08-07 13:11:49 -0400

    adding gitconfig, my-go-script.sh

---
+++ lib/my-go-script.sh
@@ -0,0 +1,43 @@
+# vim:filetype=sh
+
+# Make a function to cd to an alias
+#declare -A godir
+#go() {
+  #cd ${godir[$1]}
+#}
+#godir[loc]=~/vagrant/loc
+#godir[usc-loc]=~/vagrant/loc/data/usc
+#godir[bank-virgin-loc]=~/vagrant/loc/data/bank-virgin
+#godir[bank-wild-loc]=~/vagrant/loc/data/bank-wild
+#godir[dist-virgin-loc]=~/vagrant/loc/data/dist-virgin
+#godir[dist-wild-loc]=~/vagrant/loc/data/dist-wild
+#godir[cst-loc]=~/vagrant/loc/data/cst
+#godir[opps-virgin-loc]=~/vagrant/loc/data/opps-virgin
+#godir[opps-loc]=~/vagrant/loc/data/opps
+
+#godir[diff]=~/vagrant/diff
+#godir[usc-diff]=~/vagrant/diff/data/usc
+#godir[bank-virgin-diff]=~/vagrant/diff/data/bank-virgin
+#godir[bank-wild-diff]=~/vagrant/diff/data/bank-wild
+#godir[dist-virgin-diff]=~/vagrant/diff/data/dist-virgin
+#godir[dist-wild-diff]=~/vagrant/diff/data/dist-wild
+#godir[cst-diff]=~/vagrant/diff/data/cst
+#godir[opps-virgin-diff]=~/vagrant/diff/data/opps-virgin
+#godir[opps-diff]=~/vagrant/diff/data/opps
+
+#godir[new]=~/vagrant/new
+#godir[usc-new]=~/vagrant/new/data/usc
+#godir[bank-virgin-new]=~/vagrant/new/data/bank-virgin
+#godir[bank-wild-new]=~/vagrant/new/data/bank-wild
+#godir[dist-virgin-new]=~/vagrant/new/data/dist-virgin
+#godir[dist-wild-new]=~/vagrant/new/data/dist-wild
+#godir[cst-new]=~/vagrant/new/data/cst
+#godir[opps-virgin-new]=~/vagrant/new/data/opps-virgin
+#godir[opps-new]=~/vagrant/new/data/opps
+
+#godir[drush]=~/.composer/vendor/drush/drush
+#godir[vagrant]=~/vagrant
+#godir[kaltura]=~/vagrant/df/data/kaltura/sites/all/modules/contrib/kaltura
+#godir[usc_kaltura]=~/vagrant/df/data/kaltura/sites/all/modules/custom/usc_kaltura
+
+#godir[df]=~/vagrant/df
